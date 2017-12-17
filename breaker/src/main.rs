06be666ab89082ee25abb96b4c5ee387aac367e1
cetusDownfall <34458545+cetusDownfall@users.noreply.github.com>
2017-12-16 20:15:13 -0500

    byte size prng

--- breaker/src/main.rs
+++ breaker/src/main.rs
@@ -1,8 +1,5 @@
 extern crate util;
 
 fn main() {
-    let f: Vec<u8> = util::wolfram_30_prng(&48, &8192);
-    for i in f {
-        println!("{}", i);
-    }
+    print!("{}, {}, {}", util::random_byte(), util::random_byte(), util::random_byte());
 }
--- breaker/util/src/lib.rs
+++ breaker/util/src/lib.rs
@@ -1,49 +1,67 @@
-pub fn wolfram_30_prng(seed: &i32, length: &i32) -> Vec<u8> {
-    const RULE_NUM: i32 = 30;
-    let seed_length = 32;
-    let total_rows = 8* *length;
-    let mut out: Vec<u8> = Vec::new();
+pub fn seeded_byte(seed: u8) -> u8 {
+    const RULE_NUM: u8 = 30;
+    const SIZE: u8 = 8;
+    let mut this_row = seed;
     let mut next_row = 0;
-    let mut this_row = *seed;
-    {
-        let mut row = 0;
-        let mut next_byte = 0u8;
-        while row < total_rows {
-            for i in 0..seed_length {
-                let this_state = 
-                    if i < 31 && i > 0 { 
-                        this_row>>(i-1) & 7 
-                    } else { 
-                        (7 &
-                            ((this_row & 1) << 
-                            if i == 0 { 1 } 
-                            else { 2 } 
-                        | 
-                            (this_row & (1<<31)) >> 
-                            if i == 0 { 31 } 
-                            else { 30 } 
-                        |  
-                            if i == 0 { this_row & 4 }
-                            else { (this_row & 30)>>30 }))
-                    };
-                next_row |= ((RULE_NUM>>this_state) & 1)<<i;
-            }
-            next_byte |= ((this_row & 1)<<(row%8)) as u8;
-            for i in (0..seed_length).rev() {
-                print!("{} ", this_row>>i & 1);
-            }
-            println!("");
-            this_row = next_row;
-            next_row = 0;
-            if row%8 == 7 {
-                out.push(next_byte);
-                next_byte = 0;
-            }
-            row += 1;
+    let mut out = 0;
+    for row in 0..SIZE {
+        out |= (this_row & 1)<<row;
+        for column in 0..SIZE {
+            next_row |= (1 & (RULE_NUM>>(match column {
+                    0   =>  (this_row<<1 & 6) | (this_row>>7 & 1),
+                    7   =>  (this_row>>6 & 3) | (this_row<<2 & 1),
+                    _   =>  7 & (this_row>>(column-1)),
+                })))<<column;
         }
+        this_row = next_row;
+        next_row = 0;
     }
     out
 }
+pub fn random_byte() -> u8 {
+    seeded_byte(
+        match std::time::SystemTime::now().duration_since(std::time::UNIX_EPOCH) { 
+            Ok(n)   =>  ((n.as_secs() as u32)^n.subsec_nanos()) as u8,
+            Err(_)  =>  16,
+        })
+}
+/*pub fn wolfram_30_prng(seed: i32, length: i32) -> Vec<u8> {
+    const RULE_NUM: i32 = 30;
+    const SEED_LENGTH: i32 = 32;
+    let total_rows: usize = 8* length as usize;
+    let mut out: Vec<u8> = Vec::new();
+    let mut next_row = 0;
+    let mut this_row = seed;
+    let mut next_byte = 0;
+    for row in 0..total_rows {
+        for column in (0..SEED_LENGTH).rev() {
+            let this_state = 
+                if column < 31 && column > 0 { 
+                    this_row>>(column-1) & 7 
+                } else { 
+                    (7 &
+                        ((this_row & 1) << 
+                        if column == 0 { 1 } 
+                        else { 2 } 
+                    | 
+                        (this_row & (1<<31)) >> 
+                        if column == 0 { 31 } 
+                        else { 30 } 
+                    |  
+                        if column == 0 { this_row & 4 }
+                        else { (this_row & 30)>>30 }))
+                };
+            print!("{} ", this_row>>column & 1);
+            next_row |= ((RULE_NUM>>this_state) & 1)<<column;
+        }
+        println!("");
+        next_byte |= ((this_row & 1)<<(row%8)) as u8;
+        if row%8 == 7 { out.push(next_byte); next_byte = 0; }
+        this_row = next_row;
+        next_row = 0;
+    }
+    out
+}*/
 #[cfg(test)]
 mod tests {
     #[test]
