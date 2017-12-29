703f72f42bcf696d470d54fd73620aba43cbdcd2
cetusDownfall <34458545+cetusDownfall@users.noreply.github.com>
2017-12-29 18:45:39 -0500

    I don't know what I'm doing but it's something.

---
+++ cellular/src/lib.rs
@@ -0,0 +1,10 @@
+pub mod cellular {
+    pub struct CAPt { //to be read as Cellular Automata Point.
+        
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        assert_eq!(2 + 2, 4);
+    }
+}
